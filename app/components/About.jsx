c6dbf906a4b5f6b83e725f530a7ac21288a93c81
mdhorine <mdlhorine@gmail.com>
2017-03-29 00:38:33 +0100

    Updated About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,18 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = props => {
     return (
-        <div style={{paddingLeft:'50px'}}>
-            <h3>About</h3>
-            <p>Welcome to the About page.</p>
+        <div>
+            <h1 className='text-center'>About</h1>
+            <p>This application was created as a demo app to use the following technologies:</p>
+                <ul>
+                    <li><a href='https://facebook.github.io/react/' target='_blank'>React</a></li>
+                    <li><a href='https://github.com/ReactTraining/react-router' target='_blank'>React Router</a></li>
+                    <li><a href='http://openweathermap.org/' target='_blank'>OpenWeatherMap</a></li>
+                    <li><a href="http://foundation.zurb.com/sites/docs/" target="_blank">Foundation Sites</a></li>
+                </ul>
+            <p>The code for this application can be found in my GitHub repository: <a href='https://github.com/mdhorine/react-weather' target='_blank'>https://github.com/mdhorine/react-weather</a></p>
         </div>
     );
 };
